ab54b7c612bf26c5dedc08e2376b1e6c10ae4e78
ronaldkonjer <ronaldkonjer@gmail.com>
2015-11-12 23:45:09 +0100

    First commit

---
+++ site/src/main/java/nl/hanos/solutions/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package nl.hanos.solutions.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="myhippoproject:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
